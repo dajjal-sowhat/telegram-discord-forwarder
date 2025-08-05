d75a52c0cf2106cad791bff34c5c1841e3d63b41
vanijon <vanijon@gmail.com>
2025-08-05 19:33:56 +0330

    update

--- src/app/api/discord-icon/route.ts
+++ src/app/api/discord-icon/route.ts
@@ -1,6 +1,11 @@
 import { getBot, isDiscordClient } from "@/core/bot/client";
 import { NextRequest, NextResponse } from "next/server";
 
+declare global {
+    var GUILD_ICONS: Record<string, Response>;
+}
+global.GUILD_ICONS ||= {};
+
 export async function GET(req: NextRequest) {
     const params = Object.fromEntries(req.nextUrl.searchParams.entries());
     const bot = await getBot(params.botId);
@@ -14,18 +19,21 @@ export async function GET(req: NextRequest) {
     }
 
     if ('guild' in channel) {
+        const cache = global.GUILD_ICONS?.[channel.guild.id];
+        if (cache) return cache.clone();
         const url = channel.guild.iconURL({ size: 256, extension: 'png' }) || "http://127.0.0.1:3000/discord.png";
         const response = await fetch(url);
         if (!response.ok) {
             return NextResponse.json({error: "Failed to fetch icon"}, { status: 500 });
         }
-
-        return new Response(response.body, {
+        const final = new Response(response.body, {
             headers: {
                 'Content-Type': response.headers.get('Content-Type') || 'image/png',
                 'Cache-Control': 'public, max-age=86400' // Cache for 1 day
             }
         });
+        global.GUILD_ICONS[channel.guild.id] = final.clone();
+        return final;
     } else {
         return NextResponse.json({error: "Not a guild channel"}, { status: 400 });
     }
